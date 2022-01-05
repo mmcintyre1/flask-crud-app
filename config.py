c4127a7ba95d228a9f2f80f1d694e52fe20f7f2d
mmcintyre1 <mfmcintyre01@gmail.com>
2022-01-04 20:43:20 -0500

    changed DATABASE_HOST in DevelopmentConfig to postgres

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class ProductionConfig(Config):
     # if running on heroku, we use the database_url from the environment
     # need to hack the heroku database_url to work with sqlalchemy
     SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'DATABASE_URL').replace("://", "ql://", 1)
+        'DATABASE_URL', "").replace("://", "ql://", 1)
 
 
 class DevelopmentConfig(Config):
@@ -21,7 +21,7 @@ class DevelopmentConfig(Config):
     user = os.getenv('POSTGRES_USER', 'postgres')
     password = os.getenv('POSTGRES_PASSWORD', 'postgres')
     database = os.getenv('POSTGRES_NAME', 'postgres')
-    host = os.getenv('POSTGRES_HOST', 'localhost')
+    host = os.getenv('POSTGRES_HOST', 'postgres')
     port = os.getenv('POSTGRES_PORT', '5432')
 
     SQLALCHEMY_DATABASE_URI = f'postgresql://{user}:{password}@{host}:{port}/{database}'
