841896b68c5c3fc7c400ed749790f86fda70cac2
mmcintyre1 <mfmcintyre01@gmail.com>
2022-01-05 22:13:02 -0500

    added login manager plugin

--- silentlyfailing/__init__.py
+++ silentlyfailing/__init__.py
@@ -1,7 +1,8 @@
 
 from flask import Flask
 
-from .models import db, migrate
+from .models import db, migrate, setup_admin_user
+from .routes import sf, login_manager
 
 
 def create_app():
@@ -13,13 +14,18 @@ def create_app():
         app.config.from_object("config.DevelopmentConfig")
 
     with app.app_context():
+        # register blueprints
+        app.register_blueprint(sf)
+
+        # initialize plugins
         db.init_app(app)
         migrate.init_app(app, db)
+        login_manager.init_app(app)
+
+        # setup database
         db.create_all()
 
-    @app.route('/')
-    @app.route('/index')
-    def index():
-        return 'Hello, World!'
+        # setup admin user
+        setup_admin_user(db)
 
     return app
