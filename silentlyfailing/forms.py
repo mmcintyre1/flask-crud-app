8cb70141aefe87763a3da167a7614feed92ccef3
mmcintyre1 <mfmcintyre01@gmail.com>
2022-01-16 21:09:04 -0500

    made login form look better

--- silentlyfailing/forms.py
+++ silentlyfailing/forms.py
@@ -5,9 +5,9 @@ from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
-    """Form class for user login."""
     username = StringField('username', [validators.Length(min=4, max=25)])
     password = PasswordField('password', validators=[DataRequired()])
+    submit = SubmitField('submit')
 
 
 class PostForm(FlaskForm):
