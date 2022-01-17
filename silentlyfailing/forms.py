42eefb41f9d9c8057c41d6abd5cdf9c87c4fec43
mmcintyre1 <mfmcintyre01@gmail.com>
2022-01-16 20:06:17 -0500

    changed post submission to form

--- silentlyfailing/forms.py
+++ silentlyfailing/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, validators
+from flask_ckeditor import CKEditorField
+from wtforms import StringField, PasswordField, SubmitField, validators
 from wtforms.validators import DataRequired
 
 
@@ -7,3 +8,10 @@ class LoginForm(FlaskForm):
     """Form class for user login."""
     username = StringField('username', [validators.Length(min=4, max=25)])
     password = PasswordField('password', validators=[DataRequired()])
+
+
+class PostForm(FlaskForm):
+    title = StringField('title', [validators.Length(min=1, max=100)])
+    body = CKEditorField('body', validators=[DataRequired()])
+    author = StringField('author', [validators.Length(min=1, max=15)])
+    submit = SubmitField('submit')
